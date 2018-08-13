17bbb2cd54e7a36cac3dd3db4d1154b2d35f2f14
zetendra <jitendra1260@gmail.com>
2018-08-13 15:04:47 +0530

    Wow

---
+++ src/test/java/Testing.java
@@ -0,0 +1,19 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by zetendralingamgunta on 8/13/18.
+ */
+public class Testing {
+
+    @Test
+    public void cool() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void winter() {
+        Assert.assertTrue(true);
+    }
+
+}
